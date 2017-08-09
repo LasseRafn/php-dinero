18fee72e6cea159405b81ab22660f5305111e1f0
Lasse Rafn <lasserafn@gmail.com>
2017-08-09 11:56:16 +0200

    [Phpunit] Model toString()

--- tests/Utils/ModelTest.php
+++ tests/Utils/ModelTest.php
@@ -8,24 +8,24 @@ use LasseRafn\Dinero\Utils\Request;
 
 class ModelTest extends TestCase
 {
-	/** @var Model */
-	public $model;
-
-	public function setUp()
-	{
-		$this->model = new Model( new Request(), [ 'id' => 1, 'foo' => 'bar' ] );
-
-		parent::setUp();
-	}
-
 	/** @test */
 	public function to_array_returns_an_array_of_public_attributes()
 	{
-		$attributes = $this->model->toArray();
+		$model = new Model( new Request(), [ 'id' => 1, 'foo' => 'bar' ] );
+
+		$attributes = $model->toArray();
 
 		$this->assertArrayHasKey('id', $attributes);
 		$this->assertArrayHasKey('foo', $attributes);
 		$this->assertSame('bar', $attributes['foo']);
 		$this->assertSame(1, $attributes['id']);
+	}
+
+	/** @test */
+	public function can_return_a_json_string_if_echoed_out()
+	{
+		$model = new Model( new Request(), [ 'id' => 1, 'foo' => 'bar' ] );
+
+		$this->assertSame('{"id":1,"foo":"bar"}', (string) $model);
 	}
 }
