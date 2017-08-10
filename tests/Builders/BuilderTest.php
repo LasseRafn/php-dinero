be840b33954e9ae0f6d168365e90da29ee49e6f3
Lasse Rafn <lasserafn@gmail.com>
2017-08-10 15:35:47 +0200

    [Test] Builder getEntity()

--- tests/Builders/BuilderTest.php
+++ tests/Builders/BuilderTest.php
@@ -2,8 +2,17 @@
 
 namespace LasseRafn\Dinero\Tests\Builders;
 
+use LasseRafn\Dinero\Builders\ContactBuilder;
 use LasseRafn\Dinero\Tests\TestCase;
+use LasseRafn\Dinero\Utils\Request;
 
 class BuilderTest extends TestCase
 {
+	/** @test */
+	public function can_get_entity()
+	{
+		$builder = new ContactBuilder( new Request() );
+
+		$this->assertSame( 'contacts', $builder->getEntity() );
+	}
 }
