2464033a4f6bdffb08ce3fe1d64daae909a51901
Lasse Rafn <lasserafn@gmail.com>
2017-08-10 14:29:58 +0200

    [Test] Dinero request builders

--- tests/DineroTest.php
+++ tests/DineroTest.php
@@ -3,6 +3,10 @@
 namespace LasseRafn\Dinero\Tests;
 
 use LasseRafn\Dinero\Dinero;
+use LasseRafn\Dinero\Requests\ContactRequestBuilder;
+use LasseRafn\Dinero\Requests\CreditnoteRequestBuilder;
+use LasseRafn\Dinero\Requests\InvoiceRequestBuilder;
+use LasseRafn\Dinero\Requests\ProductRequestBuilder;
 
 class DineroTest extends TestCase
 {
@@ -17,5 +21,15 @@ class DineroTest extends TestCase
 
 		$this->assertSame('foo', $dinero->getAuthToken());
 		$this->assertSame('bar', $dinero->getOrgId());
+	}
+
+	/** @test */
+	public function can_return_builders() {
+		$dinero = new Dinero('clientId', 'clientSecret');
+
+		$this->assertSame(ContactRequestBuilder::class, get_class($dinero->contacts()));
+		$this->assertSame(InvoiceRequestBuilder::class, get_class($dinero->invoices()));
+		$this->assertSame(CreditnoteRequestBuilder::class, get_class($dinero->creditnotes()));
+		$this->assertSame(ProductRequestBuilder::class, get_class($dinero->products()));
 	}
 }
