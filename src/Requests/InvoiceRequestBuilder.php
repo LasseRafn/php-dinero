6af988ea541e184b9184e7a7a991801353ddd08a
Lasse Rafn <lasserafn@gmail.com>
2017-11-23 09:18:32 +0100

    Invoice attributes

--- src/Requests/InvoiceRequestBuilder.php
+++ src/Requests/InvoiceRequestBuilder.php
@@ -7,88 +7,75 @@ use LasseRafn\Dinero\Utils\RequestBuilder;
 
 class InvoiceRequestBuilder extends RequestBuilder
 {
-    public function __construct(Builder $builder)
-    {
-        $this->parameters['fields'] = 'Guid,ContactName,Date,Description,TotalInclVat,Currency,Status';
+	public function __construct( Builder $builder ) {
+		$this->parameters['fields'] = 'CreatedAt,UpdatedAt,DeletedAt,Guid,Date,Status,TotalInclVat,Description,ContactName,Number,Currency,PaymentDate,Type,TotalExclVat,TotalInclVatInDkk,TotalExclVatInDkk,MailOutStatus';
 
-        parent::__construct($builder);
-    }
+		parent::__construct( $builder );
+	}
 
-    public function from(\DateTime $startDate)
-    {
-        $this->parameters['startDate'] = $startDate->format($this->dateFormat);
+	public function from( \DateTime $startDate ) {
+		$this->parameters['startDate'] = $startDate->format( $this->dateFormat );
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function to(\DateTime $endDate)
-    {
-        $this->parameters['endDate'] = $endDate->format($this->dateFormat);
+	public function to( \DateTime $endDate ) {
+		$this->parameters['endDate'] = $endDate->format( $this->dateFormat );
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function search($query)
-    {
-        $this->parameters['freeTextSearch'] = $query;
+	public function search( $query ) {
+		$this->parameters['freeTextSearch'] = $query;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function onlyPaid()
-    {
-        $this->parameters['statusFilter'] = 'Paid';
+	public function onlyPaid() {
+		$this->parameters['statusFilter'] = 'Paid';
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function onlyOverPaid()
-    {
-        $this->parameters['statusFilter'] = 'OverPaid';
+	public function onlyOverPaid() {
+		$this->parameters['statusFilter'] = 'OverPaid';
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function onlyOverdue()
-    {
-        $this->parameters['statusFilter'] = 'Overdue';
+	public function onlyOverdue() {
+		$this->parameters['statusFilter'] = 'Overdue';
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function sortByVoucherNumber()
-    {
-        return $this->sortBy('VoucherNumber');
-    }
+	public function sortByVoucherNumber() {
+		return $this->sortBy( 'VoucherNumber' );
+	}
 
-    public function sortByVoucherDate()
-    {
-        return $this->sortBy('VoucherDate');
-    }
+	public function sortByVoucherDate() {
+		return $this->sortBy( 'VoucherDate' );
+	}
 
-    public function sortByStatus()
-    {
-        return $this->sortBy('Status');
-    }
+	public function sortByStatus() {
+		return $this->sortBy( 'Status' );
+	}
 
-    public function sortBy($value)
-    {
-        $this->parameters['sort'] = $value;
+	public function sortBy( $value ) {
+		$this->parameters['sort'] = $value;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function sortDescending()
-    {
-        $this->parameters['sortOrder'] = 'descending';
+	public function sortDescending() {
+		$this->parameters['sortOrder'] = 'descending';
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function sortAscending()
-    {
-        $this->parameters['sortOrder'] = 'ascending';
+	public function sortAscending() {
+		$this->parameters['sortOrder'] = 'ascending';
 
-        return $this;
-    }
+		return $this;
+	}
 }
