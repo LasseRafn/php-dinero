2676ec54fe2d07f57c0bc051203e077cf2737672
Lasse Rafn <lasserafn@gmail.com>
2017-11-23 09:18:10 +0100

    Invoice attributes

--- src/Models/Invoice.php
+++ src/Models/Invoice.php
@@ -8,45 +8,49 @@ use LasseRafn\Dinero\Utils\Model;
 
 class Invoice extends Model
 {
-    protected $entity = 'invoices';
-    protected $primaryKey = 'Guid';
+	protected $entity     = 'invoices';
+	protected $primaryKey = 'Guid';
 
-    public $PaymentDate;
-    public $PaymentStatus;
-    public $PaymentConditionNumberOfDays;
-    public $PaymentConditionType;
-    public $Status;
-    public $ContactGuid;
-    public $Guid;
-    public $TimeStamp;
-    public $CreatedAt;
-    public $UpdatedAt;
-    public $DeletedAt;
-    public $Number;
-    public $ContactName;
-    public $TotalExclVat;
-    public $TotalVatableAmount;
-    public $TotalInclVat;
-    public $TotalNonVatableAmount;
-    public $TotalVat;
+	public $PaymentDate;
+	public $PaymentStatus;
+	public $PaymentConditionNumberOfDays;
+	public $PaymentConditionType;
+	public $Status;
+	public $ContactGuid;
+	public $Guid;
+	public $TimeStamp;
+	public $CreatedAt;
+	public $UpdatedAt;
+	public $DeletedAt;
+	public $Number;
+	public $ContactName;
+	public $TotalExclVat;
+	public $TotalVatableAmount;
+	public $TotalInclVat;
+	public $TotalNonVatableAmount;
+	public $TotalVat;
 
-    /** @var array */
-    public $TotalLines;
+	/** @var array */
+	public $TotalLines;
 
-    public $Currency;
-    public $Language;
-    public $ExternalReference;
-    public $Description;
-    public $Comment;
-    public $Date;
+	public $Currency;
+	public $Language;
+	public $ExternalReference;
+	public $Description;
+	public $Comment;
+	public $Date;
 
-    /** @var array */
-    public $ProductLines;
+	public $Type;
+	public $TotalInclVatInDkk;
+	public $TotalExclVatInDkk;
+	public $MailOutStatus;
 
-    public $Address;
+	/** @var array */
+	public $ProductLines;
 
-	public function payments()
-	{
-		return new PaymentRequestBuilder(new PaymentBuilder($this->request, "invoices/{$this->Guid}/payments"));
+	public $Address;
+
+	public function payments() {
+		return new PaymentRequestBuilder( new PaymentBuilder( $this->request, "invoices/{$this->Guid}/payments" ) );
 	}
 }
