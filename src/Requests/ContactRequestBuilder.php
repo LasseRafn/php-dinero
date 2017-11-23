5a4ac30a7b2a9bfe5360e5d0a394cf12370132f4
Lasse Rafn <lasserafn@gmail.com>
2017-11-23 09:19:43 +0100

    Contact fields

--- src/Requests/ContactRequestBuilder.php
+++ src/Requests/ContactRequestBuilder.php
@@ -7,10 +7,9 @@ use LasseRafn\Dinero\Utils\RequestBuilder;
 
 class ContactRequestBuilder extends RequestBuilder
 {
-    public function __construct(Builder $builder)
-    {
-	    $this->parameters['fields'] = 'ContactGuid,CreatedAt,UpdatedAt,DeletedAt,IsDebitor,IsCreditor,ExternalReference,Name,Street,ZipCode,City,CountryKey,Phone,Email,Webpage,AttPerson,VatNumber,EanNumber,PaymentConditionType,PaymentConditionNumberOfDays,IsPerson';
+	public function __construct( Builder $builder ) {
+		$this->parameters['fields'] = 'ContactGuid,CreatedAt,UpdatedAt,DeletedAt,IsDebitor,IsCreditor,ExternalReference,Name,Street,ZipCode,City,CountryKey,Phone,Email,Webpage,AttPerson,VatNumber,EanNumber,PaymentConditionType,PaymentConditionNumberOfDays,IsPerson';
 
-        parent::__construct($builder);
-    }
+		parent::__construct( $builder );
+	}
 }
