7837c96e2eb0346a981ab001ceb8a998259c1e30
Lasse Rafn <lasserafn@gmail.com>
2018-02-08 16:47:21 +0100

    RequestException fix

--- src/Exceptions/DineroRequestException.php
+++ src/Exceptions/DineroRequestException.php
@@ -6,44 +6,49 @@ use GuzzleHttp\Exception\ClientException;
 
 class DineroRequestException extends ClientException
 {
-    public $validationErrors = [];
+	public $validationErrors = [];
 
-    public function __construct(ClientException $clientException)
-    {
-        $message = $clientException->getMessage();
+	public function __construct( ClientException $clientException ) {
+		$message = $clientException->getMessage();
 
-        if ($clientException->hasResponse()) {
-            $messageResponse = json_decode($clientException->getResponse()
-                                                            ->getBody()
-                                                            ->getContents());
+		if ( $clientException->hasResponse() ) {
+			$messageResponse = json_decode( $clientException->getResponse()->getBody()->getContents() );
 
-            $message = '';
+			if ( ! $messageResponse ) {
+				$message = $clientException->getResponse()->getBody()->getContents();
+			} else {
+				$message = '';
 
-            if (isset($messageResponse->message)) {
-                $message = "{$messageResponse->message}:";
-            } elseif (isset($messageResponse->error)) {
-                $message = "{$messageResponse->error}:";
-            }
+				if ( isset( $messageResponse->message ) ) {
+					$message = "{$messageResponse->message}:";
+				} elseif ( isset( $messageResponse->error ) ) {
+					$message = "{$messageResponse->error}:";
+				}
 
-            if (isset($messageResponse->validationErrors)) {
-                foreach ($messageResponse->validationErrors as $key => $validationError) {
-                    $this->validationErrors[$key][] = $validationError;
-                    $message .= "{$key}: {$validationError}\n";
-                }
-            }
+				if ( isset( $messageResponse->validationErrors ) ) {
+					foreach ( $messageResponse->validationErrors as $key => $validationError ) {
+						$this->validationErrors[ $key ][] = $validationError;
+						$message                          .= "{$key}: {$validationError}\n";
+					}
+				}
 
-            if (isset($messageResponse->languageSpecificMessages)) {
-                foreach ($messageResponse->languageSpecificMessages as $error) {
-                    $message .= "{$error->property}: {$error->message}\n";
-                }
-            }
-        }
+				if ( isset( $messageResponse->languageSpecificMessages ) ) {
+					foreach ( $messageResponse->languageSpecificMessages as $error ) {
+						$message .= "{$error->property}: {$error->message}\n";
+					}
+				}
 
-        $request = $clientException->getRequest();
-        $response = $clientException->getResponse();
-        $previous = $clientException->getPrevious();
-        $handlerContext = $clientException->getHandlerContext();
+				if ( $message === '' ) {
+					$message = json_encode( $messageResponse );
+				}
+			}
+		}
 
-        parent::__construct($message, $request, $response, $previous, $handlerContext);
-    }
+		$request        = $clientException->getRequest();
+		$response       = $clientException->getResponse();
+		$previous       = $clientException->getPrevious();
+		$handlerContext = $clientException->getHandlerContext();
+
+		parent::__construct( $message, $request, $response, $previous, $handlerContext );
+	}
 }
