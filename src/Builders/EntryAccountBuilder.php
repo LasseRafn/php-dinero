91c3e03543f015cde242efdc67e45dbb04995f40
Stefan Ninic <ninicstefan94@gmail.com>
2019-06-27 11:13:34 +0200

    Accounts don't return Collection or paginated response so we have to ovveride get() method inside builders, changed $request access to protected so we can override it in child classes

--- src/Builders/EntryAccountBuilder.php
+++ src/Builders/EntryAccountBuilder.php
@@ -2,11 +2,36 @@
 
 namespace LasseRafn\Dinero\Builders;
 
-use LasseRafn\Dinero\Models\Contact;
+use LasseRafn\Dinero\Exceptions\DineroRequestException;
+use LasseRafn\Dinero\Exceptions\DineroServerException;
 use LasseRafn\Dinero\Models\EntryAccount;
 
 class EntryAccountBuilder extends Builder
 {
     protected $entity = 'accounts/entry';
     protected $model = EntryAccount::class;
+
+    /**
+     * @inheritDoc
+     */
+    public function get($parameters = '')
+    {
+        try {
+            $dineroApiResponse = $this->request->curl->get("{$this->entity}{$parameters}");
+
+            $jsonResponse = json_decode($dineroApiResponse->getBody()->getContents());
+        } catch (ClientException $exception) {
+            throw new DineroRequestException($exception);
+        } catch (ServerException $exception) {
+            throw new DineroServerException($exception);
+        }
+
+        $request = $this->request;
+
+        $items = array_map(function ($item) use ($request) {
+            return new $this->model($request, $item);
+        }, $jsonResponse);
+
+        return ['items' => $items];
+    }
 }
